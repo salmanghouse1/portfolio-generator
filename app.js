a812624c2760f1e2a4c369f00c1faf4160233538
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-05 22:31:42 -0500

    9.2.4 page

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const name = profileDataArgs[0];
+
+const github = profileDataArgs[1];
 
 
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+//     console.log('================');
 
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+//     // Is the same as this...
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
 
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+const generatePage = (userName, githubName) => {
+    return `Name:${userName}
+    Github: ${githubName}`;
+}
+
+console.log(generatePage(name, github));

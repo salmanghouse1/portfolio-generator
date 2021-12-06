c7d7029cec382488cf0a782d8e0b974a8e9cb0fa
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-06 13:13:32 -0500

    modularize the code

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const name = profileDataArgs[0];
+const profileDataArgs = process.argv.slice(2);
 
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
+
+const generatePage = (name, github) => {
+    return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
 
 
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('================');
-
-//     // Is the same as this...
-//     profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
 
 
-// printProfileData(profileDataArgs);
 
 
-const generatePage = (userName, githubName) => {
-    return `Name:${userName}
-    Github: ${githubName}`;
-}
 
-console.log(generatePage(name, github));
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
